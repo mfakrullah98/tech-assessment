cd0d307d5b11b17b78417bd6899d6bd2e2423e16
mfakrullah98 <mfakrullah98@gmail.com>
2024-02-14 13:32:49 +0800

    feat: serviceB index

---
+++ services/serviceB/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { connect } = require('../../rabbitmq');
+
+const app = express();
+const port = 3002;
+
+(async () => {
+    const { connection, channel } = await connect();
+    const exchange = 'user_events';
+
+    channel.assertExchange(exchange, 'topic', { durable: true });
+    const queue = await channel.assertQueue('', { exclusive: true });
+
+    channel.bindQueue(queue.queue, exchange, 'user.profile.updated');
+
+    channel.consume(queue.queue, (msg) => {
+        const userData = JSON.parse(msg.content.toString());
+        console.log('Received User Update:', userData);
+    }, { noAck: true });
+})();
+
+app.listen(port, () => {
+    console.log(`Service B listening on port ${port}`);
+});
